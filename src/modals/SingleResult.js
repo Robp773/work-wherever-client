5410384deb5bbe588b52c0a9a0cd6bbcd86fffa2
Your Name <robpeterman573@gmail.com>
2017-11-07 18:02:31 -0500

    connected state to SingleResult component

--- src/modals/SingleResult.js
+++ src/modals/SingleResult.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './SingleResult.css';
 import {Link} from 'react-router-dom';
-export default function SingleResult(props){
+import {connect} from 'react-redux'
+export class SingleResult extends React.Component{
+	super(props){
+		constructor(props);
+	}
+   
     
-    return(
-        <div className="resultModal">
+		render(){
+			const item = this.props.result;
+			const oneResult = (
+			<div className='result'>
+				<h4>Name</h4>
+					<div>{item.name}</div>
+				<h4>Location</h4>
+					<div>{item.location}</div>
+				<h4>Environment</h4>
+					<div>{item.environment}</div>
+				<h4>Internet</h4>
+					<div>{item.internet.join(', ')}</div>
+				<h4>Services</h4>
+					<div>{item.services.join(', ')}</div>
+			</div>
+			)
+				
+			
+			
+			return(
+			   <div className="resultModal">
 			<div className="singleResultBody">
 			<Link to='/'><button className='exitBtn'>X</button></Link>
 			
@@ -13,40 +37,17 @@ export default function SingleResult(props){
         <img className="resultMap" alt='Google Map'
         src="https://cnet2.cbsistatic.com/img/H_zPLL8-QTZOLxJvgHQ1Jkz0EgY=/830x467/2013/07/10/f0bcef02-67c2-11e3-a665-14feb5ca9861/maps_routemap.png"/>
         </div>
-		<div className="result location">
-			<h4>Location</h4>
-			<div>
-				200 Sweet Spot Road
-			</div>
-			<div>
-				Tabawakasada City OH 04938
-			</div>
-		</div>
-		<div className="result details">
-			<h4>Environment</h4>
-			<div>
-				Some Activity
-			</div>
-			<div>
-				<h4>Internet Access</h4>
-				<div>
-					WiFi - Ethernet
-				</div>
-			</div>
-			<div>
-				<h4>Services</h4>
-				<div>
-					Coffee - Outlets
-				</div>
-			</div>
-			<div>
-				<h4>Comments</h4>
-				<div>
-					Nice low key place to get some work done
-				</div>
-			</div>
-		</div>
+		<div>{oneResult}</div>
 		</div>
 	</div>
-    )
+	)
+		}
+     
 }
+
+
+const mapStateToProps = state => ({    
+    result: state.singleResult
+    });        
+        
+export default connect(mapStateToProps)(SingleResult);
